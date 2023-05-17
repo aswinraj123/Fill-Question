a4426f4b089cf67800e2a8803c66ccc40370dd43
aswinraj123 <aswinrajchuzhali@gmail.com>
2023-05-17 20:46:12 +0530

    first commit

---
+++ fill the blanks.py
@@ -0,0 +1,32 @@
+print("welcome to short 5 fill in blanks biology questions")
+
+an1="liver"
+an2="liver biopsy"
+an3="capsid"
+an4="virion"
+an5="alveoli"
+
+question1="1. Albumin is the most important protein in blood serum. It is produced by " 
+print("\n",question1)
+A=input("enter your answer : ").lower()
+print(f"CORRECT \n Answer is : {question1}{an1}" if A==an1 else f"WRONG \n Answer is : {question1}{an1} ")
+
+question2="2. Cirrhosis is a liver disease described by the abnormal structure and functioning of the liver. This is mainly diagnosed by "
+print("\n",question2)
+A=input("enter your answer : ").lower()
+print(f"CORRECT \n Answer is : {question2}{an2}" if A==an2 else f"WRONG \n Answer is : {question2}{an2} ")
+
+question3="3. The protein coat of viruses that enclose the genetic material is called "
+print("\n",question3)
+A=input("enter your answer : ").lower()
+print(f"CORRECT \n Answer is : {question3}{an3}" if A==an3 else f"WRONG \n Answer is : {question3}{an3} ")
+
+question4="4. A fully formed infectious viral particle is called "
+print("\n",question4)
+A=input("enter your answer : ").lower()
+print(f"CORRECT \n Answer is : {question4}{an4}" if A==an4 else f"WRONG \n Answer is : {question4}{an4} ")
+
+question5="5.  The tiny air sacs present in human lungs is called "
+print("\n",question5)
+A=input("enter your answer : ").lower()
+print(f"CORRECT \n Answer is : {question5}{an5}" if A==an5 else f"WRONG \n Answer is : {question5}{an5} ")
